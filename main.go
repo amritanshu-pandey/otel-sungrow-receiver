e35951280eef42df93b1ce61a6b4b88f27f3edfd
Amritanshu Pandey <email@amritanshu.in>
2025-02-22 23:20:37 +1100

    fix scaling bug

--- main.go
+++ main.go
@@ -77,17 +77,17 @@ func pollInverter(client modbus.Client, config *Config, metricsMap map[string]pr
 			var value float64
 			switch metric.Type {
 			case "U16":
-				value = float64(uint32(results[0])<<8|uint32(results[1])) * scale
+				value = float64(uint32(results[0])<<8|uint32(results[1]))
 			case "S16":
 				fmt.Println(results)
 				rawValue := int16(results[0])<<8 | int16(results[1])
-				value = float64(rawValue) * scale
+				value = float64(rawValue)
 			case "U32":
-				value = float64(uint32(results[0])<<8|uint32(results[1])) * scale
+				value = float64(uint32(results[0])<<8|uint32(results[1]))
 			case "S32":
 				fmt.Println(results)
 				rawValue := int32(results[0])<<24 | int32(results[1])<<16 | int32(results[2])<<8 | int32(results[3])
-				value = float64(rawValue) * scale
+				value = float64(rawValue)
 			}
 
 			// Apply scaling
